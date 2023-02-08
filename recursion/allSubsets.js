bb5542c4fa526f13b8c24e2e7ea057ecbb5ff95d
Daniele Pellegrini <10761843+dmpellegrini@users.noreply.github.com>
2023-02-08 01:57:51 -0500

    Added a few recursion problems to the mix

---
+++ recursion/allSubsets.js
@@ -0,0 +1,27 @@
+set = [1,2]
+
+function all_subsets (arr){
+
+  let subSet = [] 
+  for (let i = 0; i < arr.length; i++) {
+    subSet[i] = null
+  }
+
+  createSubset(arr, subSet, 0)
+}
+
+function createSubset(arr, subSet, index){
+
+  if (index === arr.length) {
+    console.log(subSet)
+  }else{
+    subSet[index] = null
+    createSubset(arr, subSet, index + 1)
+    subSet[index] = arr[index]
+    createSubset(arr, subSet, index + 1)
+  }
+
+}
+  
+
+all_subsets(set)

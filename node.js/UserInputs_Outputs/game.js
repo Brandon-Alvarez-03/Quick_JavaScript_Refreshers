d213e7d1851b3755af3f3ef62af995f41ade692f
Brandon Alvarez <balvarez325@gmail.com>
2023-02-27 22:39:23 -0500

    update stdin/stdout example to include detailed comments

--- node.js/UserInputs_Outputs/game.js
+++ node.js/UserInputs_Outputs/game.js
@@ -1,24 +1,43 @@
+// Generate a random number between 1 and 10, and convert it to a string.
 let secretValue = Math.floor(1 + Math.random() * 10).toString();
 
+// Create an array of strings representing the numbers 1 through 10.
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
+// Export an object that contains a function called 'testNumber'.
 module.exports = {
   testNumber: (input) => {
+    // If the user types 'quit', exit the program.
     if (input === "quit") {
       process.stdout.write("Ok. Bye!\n");
       process.exit();
     }
+    // If the input is not a number from 1 through 10, prompt the user to guess again.
     if (!numbers.includes(input)) {
       process.stdout.write(
         "Choose a number from 1 through 10!\nIs the number ... "
       );
     } else if (input === secretValue) {
+      // If the input matches the secret number, exit the program.
       process.stdout.write(
         "Woah you got it! Are you psychic? See you later!\n"
       );
       process.exit();
     } else {
+      // If the input is a number from 1 through 10 but does not match the secret number, prompt the user to guess again.
       process.stdout.write("Nope. Guess again!\nIs the number ... ");
     }
   },
 };
+
+
+/*
+This code exports an object that contains a function called testNumber. The function takes an input argument and performs the following steps:
+
+1. If the user types 'quit', exit the program.
+2. If the input is not a number from 1 through 10, prompt the user to guess again.
+3. If the input matches the secret number, exit the program.
+4. If the input is a number from 1 through 10 but does not match the secret number, prompt the user to guess again.
+
+The secret number is generated randomly using the Math.random() and Math.floor() methods, and is stored as a string in the secretValue variable. The array of strings representing the numbers 1 through 10 is stored in the numbers variable.
+*/
--- node.js/UserInputs_Outputs/stdin_example.js
+++ node.js/UserInputs_Outputs/stdin_example.js
@@ -1,12 +1,25 @@
+/*
+This code imports a function called 'testNumber' from a file called 'game.js'. The function prompts the user to guess a number between 1 and 10.
+If the user types 'quit', the program will exit. Otherwise, the input will be passed to the 'testNumber' function to check if it matches the secret number.
+
+1. The 'process.stdout.write' method writes a message to the console prompting the user to guess a number between 1 and 10, and provides instructions on how to quit the program.
+2. The 'playGame' function is defined to take the user input, convert it to a string, and pass it to the 'testNumber' function.
+3. The 'process.stdin.on' method listens for data input from the user and calls the 'playGame' function with the user input as an argument.
+*/
+
+// Import the 'testNumber' function from a file called 'game.js'.
 let {testNumber} = require("./game.js");
 
+// Prompt the user to guess a number between 1 and 10, and provide instructions on how to quit the program.
 process.stdout.write(
   'I\'m thinking of a number from 1 through 10. What do you think it is? \n(Write "quit" to give up.)\n\nIs the number ... '
 );
 
+// Define a function called 'playGame' that takes the user input, converts it to a string, and passes it to the 'testNumber' function.
 let playGame = (userInput) => {
   let input = userInput.toString().trim();
   testNumber(input);
 };
 
+// Listen for data input from the user, and call the 'playGame' function with the user input as an argument.
 process.stdin.on("data", playGame);
